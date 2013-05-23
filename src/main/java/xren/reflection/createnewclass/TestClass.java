ae4399879908c7003daa86ff43bf0e752085780b
unknown <xiang.ren@GTS-D0024.gtspt.corp>
2013-05-23 16:50:32 +0100

    first commit

---
+++ src/main/java/xren/reflection/createnewclass/TestClass.java
@@ -0,0 +1,22 @@
+package xren.reflection.createnewclass;
+
+public class TestClass {
+	
+	private int v1;
+	
+	public TestClass() {
+		System.out.println("TestClass Constructor");
+	}
+	
+	
+	public int getV1() {
+		return v1;
+	}
+
+	public void setV1(int v1) {
+		this.v1 = v1;
+	}
+
+	
+
+}
