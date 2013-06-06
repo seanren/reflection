dd2cf994382610ad537b370c5c29214fa3cde69f
xiang.ren <xren@live.com>
2013-06-06 16:21:30 +0100

    rename the package

---
+++ src/main/java/xren/reflection/instanceclass/Main.java
@@ -0,0 +1,45 @@
+package xren.reflection.instanceclass;
+
+import java.lang.reflect.Constructor;
+
+public class Main {
+
+	public Main() {
+		System.out.println("main.constructor");
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args)  {
+		try {
+			Class <TestClass> clazz = (Class<TestClass>) Class.forName("xren.reflection.createnewclass.TestClass");
+			TestClass test = loadClass(clazz);
+			test.setV1(10);
+			System.out.println("TestClass output " + test.getV1());
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+	public static <T> T loadClass(Class<T> clazz) {
+
+		T instance = null;
+
+		try {
+
+			Constructor<T> constructor = clazz.getDeclaredConstructor(new Class[0]);
+			instance = constructor.newInstance();
+			return instance;
+
+		} catch (Exception e) {
+			throw new RuntimeException("Unable to create class " + clazz.getName());
+		}
+
+	}
+	
+	
+
+}
